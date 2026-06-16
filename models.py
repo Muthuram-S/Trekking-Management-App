86c910172ff1cb6f2f167dcecba6642e710391bc
Muthuramseenivasan <2401116025@ptuniv.edu.in>
2026-06-16 18:03:48 +0530

    Add gitignore and update models

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     Password=db.Column(db.String(100),unique=False,nullable=False)
     Role=db.Column(db.String(100),nullable=False)
     Status=db.Column(db.String(100),nullable=False)
-    bookkigs=db.relationship('booking',backref='user')
+    bookigs=db.relationship('Book',backref='user')
 
 class Trek(db.Model):
     Trek_Id=db.Column(db.Integer,primary_key=True)
@@ -18,18 +18,18 @@ class Trek(db.Model):
     Location=db.Column(db.String(100),nullable=False)
     Duration=db.Column(db.Integer,nullable=False)
     Difficulty=db.Column(db.String(100),nullable=False)
-    Available_Staff=db.Column(db.String(100),nullable=False)
-    Staff_Id=db.Column(db.String(100),nullable=False)
+    Available_Staff=db.Column(db.Integer,nullable=False)
+    Staff_Id=db.Column(db.Integer,db.ForeignKey('user.Id'))
     Status=db.Column(db.String(100),nullable=False)
     Start_Date=db.Column(db.DateTime,nullable=False)
     End_Date=db.Column(db.DateTime,nullable=False)
-    Description=db.Column(db.String(100),nullable=False)
-    bookkings=db.relationship('booking',backref='trek')
+    Description=db.Column(db.Text,nullable=False)
+    bookings=db.relationship('Book',backref='trek')
 
 
 class Book(db.Model):
     booking_id=db.Column(db.Integer,primary_key=True)
     User_Id=db.Column(db.Integer,db.ForeignKey('user.Id'))
     Trek_Id=db.Column(db.Integer,db.ForeignKey('trek.Trek_Id'))
-    Booking_Date=db.Column(db.DateTime,nullable=False,default=dt.utcnow)
+    Booking_Date=db.Column(db.DateTime,nullable=False,default=dt.now)
     Status=db.Column(db.String,nullable=False)
